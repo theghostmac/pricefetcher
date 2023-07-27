93d508db8b3272765bb512ae9ad5e923a9a0f0ff
theghostmac <theghostmac@gmail.com>
2023-07-27 15:12:31 +0100

    feature:

--- common/logging.go
+++ common/logging.go
@@ -1,11 +1,24 @@
 package common
 
-import "github.com/theghostmac/pricefetcher/internal/app"
+import (
+	"context"
+	"github.com/sirupsen/logrus"
+	"github.com/theghostmac/pricefetcher/internal/app"
+	"time"
+)
 
 type LoggingService struct {
 	next app.PriceFetcher
 }
 
-func (ls *LoggingService) Fetch() {
+func (ls *LoggingService) FetchPrice(ctx context.Context, ticker string) (price float64, err error) {
+	defer func(begin time.Time) {
+		logrus.WithFields(logrus.Fields{
+			"took":  time.Since(begin),
+			"err":   err,
+			"price": price,
+		}).Info("fetchPrice")
+	}(time.Now())
 
+	return ls.next.FetchPrice(ctx, ticker)
 }
