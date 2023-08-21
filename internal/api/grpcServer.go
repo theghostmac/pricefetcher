727ef1aa24248c5af7b529374c6fa2c835cd208a
theghostmac <theghostmac@gmail.com>
2023-08-21 10:15:51 +0100

    feature: created bindings for the pricefetcher protobuf

--- internal/api/grpcServer.go
+++ internal/api/grpcServer.go
@@ -1,15 +1,30 @@
 package api
 
-import "github.com/theghostmac/pricefetcher/internal/app"
+import (
+	"context"
+	"github.com/theghostmac/pricefetcher/internal/app"
+	"github.com/theghostmac/pricefetcher/proto"
+)
 
-type GRPCPriceFetcher struct {
+type GRPCPriceFetcherServer struct {
 	service app.PriceFetcher
 }
 
-func NewGRPCPriceFetcher(service app.PriceFetcher) *GRPCPriceFetcher {
-	return &GRPCPriceFetcher{
+func NewGRPCPriceFetcher(service app.PriceFetcher) *GRPCPriceFetcherServer {
+	return &GRPCPriceFetcherServer{
 		service: service,
 	}
 }
 
-func (pr *GRPCPriceFetcher) Name() *Gr
+func (grpcs *GRPCPriceFetcherServer) FetchPrice(ctx context.Context, request *proto.PriceRequest) (*proto.PriceResponse, error) {
+	price, err := grpcs.service.FetchPrice(ctx, request.Ticker)
+	if err != nil {
+		return nil, err
+	}
+
+	response := &proto.PriceResponse{
+		Ticker: request.Ticker,
+		Price:  float32(price),
+	}
+	return response, err
+}
