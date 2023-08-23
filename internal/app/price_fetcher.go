f54a5375e15650454dd8839ff5819b63942f936e
theghostmac <theghostmac@gmail.com>
2023-08-23 06:57:16 +0100

    feature: implemented real-time price fetching from CoinMarketCap

--- internal/app/price_fetcher.go
+++ internal/app/price_fetcher.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/theghostmac/pricefetcher/common"
+	"io"
 	"net/http"
 	"strconv"
 	"time"
@@ -37,52 +39,75 @@ func SimulatedPriceFetcher(ctx context.Context, ticker string) (float64, error) 
 	return price, nil
 }
 
-const binanceAPIBaseURl = "https://api.binance.com/api/v3/ticker/price"
+const coinMarketCapAPIBaseURL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
 
-type FetchPriceFromBinance struct {
-	APIKey string // Binance API key here.
+type FetchPriceFromCoinMarketCap struct {
+	APIKey string // CoinMarketCap API key here.
 }
 
-func NewFetchPriceFromBinance(apiKey string) *FetchPriceFromBinance {
-	return &FetchPriceFromBinance{
+func NewFetchPriceFromCoinMarketCap(apiKey string) *FetchPriceFromCoinMarketCap {
+	return &FetchPriceFromCoinMarketCap{
 		APIKey: apiKey,
 	}
 }
 
-func (fpfb *FetchPriceFromBinance) FetchPrice(ctx context.Context, ticker string) (float64, error) {
-	return fpfb.BinancePriceFetcher(ctx, ticker)
+func (fpfcmc *FetchPriceFromCoinMarketCap) FetchPrice(ctx context.Context, ticker string) (float64, error) {
+	return fpfcmc.CoinMarketCapPriceFetcher(ctx, ticker)
 }
 
-type BinancePriceResponse struct {
-	Symbol string `json:symbol`
-	Price  string `json:price`
+type CoinMarketCapPriceResponse struct {
+	Data []struct {
+		Symbol string `json:"symbol"`
+		Quote  struct {
+			USD struct {
+				Price string `json:"price"`
+			} `json:"USD"`
+		} `json:"quote"`
+	} `json:"data"`
 }
 
-func (fpfb *FetchPriceFromBinance) BinancePriceFetcher(ctx context.Context, ticker string) (float64, error) {
+func (fpfcmc *FetchPriceFromCoinMarketCap) CoinMarketCapPriceFetcher(ctx context.Context, ticker string) (float64, error) {
 	client := &http.Client{}
-	request, err := http.NewRequest("GET", fmt.Sprintf("%s?symbol=%sUSDT", binanceAPIBaseURl, ticker), nil)
+	req, err := http.NewRequest("GET", coinMarketCapAPIBaseURL, nil)
+	if err != nil {
+		return 0, err
+	}
+	q := req.URL.Query()
+	q.Add("start", "1")
+	q.Add("limit", "100")
+	q.Add("convert", "USD")
+	req.URL.RawQuery = q.Encode()
+
+	req.Header.Add("X-CMC_PRO_API_KEY", fpfcmc.APIKey)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			common.LogError(err)
+		}
+	}(resp.Body)
+
+	var priceResponse CoinMarketCapPriceResponse
+	err = json.NewDecoder(resp.Body).Decode(&priceResponse)
 	if err != nil {
 		return 0, err
 	}
 
-	response, err := client.Do(request)
-	if err != nil {
-		return 0, err
-	}
-	defer response.Body.Close()
-
-	var priceResponse BinancePriceResponse
-	err = json.NewDecoder(response.Body).Decode(&priceResponse)
-	if err != nil {
-		return 0, err
+	for _, coin := range priceResponse.Data {
+		if coin.Symbol == ticker {
+			price, err := ParseFloat(coin.Quote.USD.Price)
+			if err != nil {
+				return 0, err
+			}
+			return price, nil
+		}
 	}
 
-	price, err := ParseFloat(priceResponse.Price)
-	if err != nil {
-		return 0, err
-	}
-
-	return price, nil
+	return 0, fmt.Errorf("ticker (%s) not found in CoinMarketCap response", ticker)
 }
 
 func ParseFloat(input string) (float64, error) {
