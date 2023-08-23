5dcd6f9241d301ee6e219c093ccacad0688d4789
theghostmac <theghostmac@gmail.com>
2023-08-23 07:57:02 +0100

    feature: wrote tests for json api logic

--- tests/unit/jsonAPI_test.go
+++ tests/unit/jsonAPI_test.go
@@ -1,1 +1,40 @@
-package unit
+package unit_test
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"github.com/theghostmac/pricefetcher/internal/api"
+	"github.com/theghostmac/pricefetcher/internal/domain"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type mockPriceFetcher struct {
+}
+
+func (m *mockPriceFetcher) FetchPrice(ctx context.Context, ticker string) (float64, error) {
+	return 1875.91, nil
+}
+
+func TestJSONAPIServer_HandleFetchPrice(t *testing.T) {
+	mockService := &mockPriceFetcher{}
+	jsonServer := &api.JSONAPIServer{Service: mockService}
+
+	request := httptest.NewRequest(http.MethodGet, "/?ticker=ETH", nil)
+
+	recorder := httptest.NewRecorder()
+
+	err := jsonServer.HandleFetchPrice(context.Background(), recorder, request)
+	assert.NoError(t, err)
+
+	assert.Equal(t, http.StatusOK, recorder.Code)
+
+	var response domain.PriceResponse
+	err = json.NewDecoder(recorder.Body).Decode(&response)
+	assert.NoError(t, err)
+
+	assert.Equal(t, "ETH", response.Ticker)
+	assert.Equal(t, 1875.91, response.Price)
+}
